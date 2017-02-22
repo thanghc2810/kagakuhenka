a63ef816ee9b44e5d99207ad4be160d9bb965d61
Hoang Cong Thang <Hoang Cong Thang>
2017-02-23 03:00:53 +0900

    Thang edit index.html

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,13 +1,23 @@
 //player
-function seekToTimer() {
-	  var myPlayer;
+var myPlayer;
+function seekToTimer(time) {
+	  
 	  videojs('vid1').ready(function() {
 	    myPlayer = this;
 	    // myPlayer.src({ type: 'video/youtube', src: 'https://www.youtube.com/watch?v=H90NdI42xNc' });
-	    myPlayer.play();
-	    myPlayer.currentTime(500);
+	    
+	    myPlayer.currentTime(time);
+        myPlayer.play();
 	    console.log("clicked");
 	  });
+}
+function showSlide(){
+
+        console.log(myPlayer.currentTime());
+    $("#test-content").html("Show Slide");
+}
+function showTest(){
+    $("#test-content").html("Show Test");   
 }
 
 //for side bar
@@ -30,17 +40,110 @@ $(document).ready(function(){
 });
 
 // show avatar in progress bar
+//   $( document ).ready(function() {
+//     var div = document.createElement('div');
+//     div.className = 'comment-user-light';
+//     div.innerHTML = "<a href='#cmt01'><img src='./public/img/avatar1.jpg' height='20' width='20'></a>"
+//     document.getElementsByClassName("vjs-control-bar")[0].appendChild(div);
+// });
+
+//show slide
+$(document).ready(function() {
+    $( ".vjs-big-play-button" ).click(function() {
+        console.log("vao ham");
+      seekToTimer("0");
+      setTimeout(function(){
+        console.log("vao ham hihi");
+          console.log(myPlayer.currentTime());
+            if(myPlayer.currentTime() == 20){
+                $("#test-content").html("Show Slide");
+            }
+            if(myPlayer.currentTime() == 90){
+                $("#test-content").html("Show Test");
+            }    
+}, 15000);
+  
+    });
+});
+
+
+// show avatar in progress bar
   $( document ).ready(function() {
     var div = document.createElement('div');
-    div.className = 'comment-user-light';
+    div.className = 'comment-user-light1';
     div.innerHTML = "<a href='#cmt01'><img src='./public/img/avatar1.jpg' height='20' width='20'></a>"
     document.getElementsByClassName("vjs-control-bar")[0].appendChild(div);
 });
 
   $( document ).ready(function() {
 	$(".vjs-volume-menu-button-horizontal").hover(function(){
-	    $(".comment-user-light").animate({"margin-left":"160px"},400);
+	    $(".comment-user-light1").animate({"margin-left":"230px"},400);
 		    }, function(){
-		    $(".comment-user-light").animate({"margin-left":"80px"},400);
+		    $(".comment-user-light1").animate({"margin-left":"150px"},400);
 		});
 });
+
+
+// show avatar in progress bar
+  $( document ).ready(function() {
+    var div = document.createElement('div');
+    div.className = 'comment-user-light2';
+    div.innerHTML = "<a href='#cmt02'><img src='./public/img/avatar2.jpg' height='20' width='20'></a>"
+    document.getElementsByClassName("vjs-control-bar")[0].appendChild(div);
+});
+  
+  $( document ).ready(function() {
+    $(".vjs-volume-menu-button-horizontal").hover(function(){
+        $(".comment-user-light2").animate({"margin-left":"300px"},400);
+            }, function(){
+            $(".comment-user-light2").animate({"margin-left":"220px"},400);
+        });
+});
+
+// show avatar in progress bar
+  $( document ).ready(function() {
+    var div = document.createElement('div');
+    div.className = 'comment-user-light3';
+    div.innerHTML = "<a href='#cmt03'><img src='./public/img/avatar3.jpg' height='20' width='20'></a>"
+    document.getElementsByClassName("vjs-control-bar")[0].appendChild(div);
+});
+  
+  $( document ).ready(function() {
+    $(".vjs-volume-menu-button-horizontal").hover(function(){
+        $(".comment-user-light3").animate({"margin-left":"370px"},400);
+            }, function(){
+            $(".comment-user-light3").animate({"margin-left":"290px"},400);
+        });
+});  
+
+// show content check
+  $( document ).ready(function() {
+    var div = document.createElement('div');
+    div.className = 'comment-user-light4';
+    div.innerHTML = "<a href='#' onclick='showSlide();'><img src='./public/img/test.png' height='20' width='20'></a>"
+    document.getElementsByClassName("vjs-control-bar")[0].appendChild(div);
+});
+  
+  $( document ).ready(function() {
+    $(".vjs-volume-menu-button-horizontal").hover(function(){
+        $(".comment-user-light4").animate({"margin-left":"240px"},400);
+            }, function(){
+            $(".comment-user-light4").animate({"margin-left":"180px"},400);
+        });
+});  
+
+// show slide
+  $( document ).ready(function() {
+    var div = document.createElement('div');
+    div.className = 'comment-user-light5';
+    div.innerHTML = "<a href='#' onclick='showTest();'><img src='./public/img/slide.png' height='20' width='20'></a>"
+    document.getElementsByClassName("vjs-control-bar")[0].appendChild(div);
+});
+  
+  $( document ).ready(function() {
+    $(".vjs-volume-menu-button-horizontal").hover(function(){
+        $(".comment-user-light5").animate({"margin-left":"530px"},400);
+            }, function(){
+            $(".comment-user-light5").animate({"margin-left":"520px"},400);
+        });
+});    
